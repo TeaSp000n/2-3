0cc3e91e7d55c95930f4a5e9174bfe46e46261cd
Олександр <@DESKTOP-S660HFA>
2022-02-18 14:03:29 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp3/OperateCost.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    abstract class OperateCost
+    {
+        public double salary;
+        public double tax;
+
+        public abstract void ApplyBonus(double bonus, double grade);
+        public abstract void ApplyTax();
+    }
+
+    class Slave : OperateCost
+    {
+        public override void ApplyBonus(double bonus, double grade)
+        {
+            salary = (10000 + 2 * bonus) * grade;
+        }
+
+        public override void ApplyTax()
+        {
+            tax = salary * 0.34;
+        }
+    }
+
+    class Warden : OperateCost
+    {
+        public override void ApplyBonus(double bonus, double grade)
+        {
+            salary = (100000 + 5 * bonus) * grade;
+        }
+        public override void ApplyTax()
+        {
+            tax = salary * 0.34;
+        }
+    }
+
+    class Lord : OperateCost
+    {
+        public override void ApplyBonus(double bonus, double grade)
+        {
+            salary = (1000000 + 10 * bonus) * grade;
+        }
+        public override void ApplyTax()
+        {
+            tax = salary * 0.34;
+        }
+    }
+}
---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OperateCost oc = new Warden();
+            Employee emp = new Employee("Petya", "Ivanov", "15.03.2002", oc);
+            double grade = Employee.DiscoverGrade("15.03.2002");
+            Console.WriteLine("Name: {0}, Surname: {1}, Date of Hire: {2}, Positoin: {3}", emp.name, emp.surname, Employee.dateOfHire, emp.operationCost);
+            oc.ApplyBonus(25000, grade);
+            oc.ApplyTax();
+            Console.WriteLine("Salary: {0}, Tax: {1}", emp.operationCost.salary, emp.operationCost.tax);
+            Console.ReadKey();
+        }
+    }
+}
